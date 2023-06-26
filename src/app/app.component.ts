52751850d49da47c1fb825571315ad74e6409dce
k_z.gebo@icloud.com <k_z.gebo@icloud.com>
2023-06-26 17:18:07 +1000

    One-Way Vs Two-Way Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngIntro';
+export class AppComponent implements AfterViewInit {
+
+  title = 'Post Form';
+  parentMessage: string = "Message coming from parent component";
+
+  message: string | undefined;
+  fromChildOutput: string | undefined;
+
+  // -----------------------------------------------------------
+
+  postTitle: string | any;
+  postDetails: string | any;
+  imgURL: string | any;
+  postUrl: string | any;
+  addBackground: boolean = false;
+
+  @ViewChild(PostComponent) childComp: any;
+
+  constructor() {
+    // console.log(this.childComp);
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.childComp);
+    // this.message = this.childComp.childMessage; 
+  }
+
+  receiveMessage($event: any) {
+    console.log($event);
+    this.fromChildOutput = $event;
+  }
 }
